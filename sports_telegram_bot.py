d1757d8bcf4ab4d7a4e5143ab91de0283edf8bc7
Shaigoldbourt22 <118125561+Shaigoldbourt22@users.noreply.github.com>
2023-11-13 14:00:07 +0200

    Update sports_telegram_bot.py

--- sports_telegram_bot.py
+++ sports_telegram_bot.py
@@ -2,6 +2,85 @@
 import telebot
 import requests
 
+# a ListNode class for the cache
+class ListNode:
+
+    def __init__(self, value = -1, prev=None, next=None):
+        self.next = next
+        self.prev = prev
+        self.value = value
+
+
+# LRU Cache class to keep users who recently sent a massage
+class Cache:
+
+    def __init__(self):
+        self.capacity = 10
+        self.linked_list_head = ListNode()  # head_dummy
+        self.linked_list_tail = self.linked_list_head
+        self.linked_list_tail = self.linked_list_tail
+        self.key_to_value = {}
+        self.key_to_node = {}
+        self.size = 0
+
+    def get_value(self, key: int) -> dict:
+        if key not in self.key_to_value:  # key is not in the dict
+            return -1
+
+        else:  # key is in the dict, move the node to the end of the linked list
+            self.remove_from_linked_list_and_update_value(key, self.key_to_value[key])
+            self.add_to_linked_list_and_update_node(key, self.key_to_value[key])
+
+
+            return self.key_to_value[key]
+
+    def put_or_update(self, key: int, value: dict) -> None:
+
+        # remove the node from the list if the key is in the dict
+        if key in self.key_to_value:
+            self.remove_from_linked_list_and_update_value(key, value)
+
+        # add the key if not in the dict
+        else:
+            self.size += 1
+            self.key_to_value[key] = value
+
+        # create the new node and add to the end of the list
+        self.add_to_linked_list_and_update_node(key, value)
+
+        # remove the first node from the linked list if the size is bigger than the capacity
+        if self.size > self.capacity:
+            node_to_remove = self.linked_list_head.next
+            self.key_to_value.pop(node_to_remove.value)
+            self.key_to_node.pop(node_to_remove.value)
+
+            self.linked_list_head.next = node_to_remove.next
+
+            if node_to_remove.next:
+                node_to_remove.next.prev = self.linked_list_head
+
+            else:  # node_to_remove wat the tail
+                self.linked_list_tail = self.linked_list_head
+
+    def remove_from_linked_list_and_update_value(self, key: str, value: dict) -> None:
+        self.key_to_value[key] = value
+        node = self.key_to_node[key]
+        node.prev.next = node.next
+
+        if node.next:
+            node.next.prev = node.prev
+
+        else:  # node is the tail
+            self.linked_list_tail = node.prev
+
+    def add_to_linked_list_and_update_node(self, key: str, value: dict) -> None:
+        new_node = ListNode(key)
+        self.linked_list_tail.next = new_node
+        new_node.prev = self.linked_list_tail
+        self.key_to_node[key] = new_node
+        self.linked_list_tail = new_node
+
+
 # telegram api info
 telegram_token = "6840332720:AAH3TnoPdotUZEanvXK4Simz9hYwDSrBV5s"
 bot = telebot.TeleBot(telegram_token)
@@ -13,7 +92,7 @@ football_api_key = "3"
 basketball_url = "https://www.balldontlie.io/api/v1/players"
 
 # basketball functions
-def get_nba_player_team(player_name):  # # gets a player's name, and return his team
+def get_nba_player_team(player_name: str) -> str:  # # gets a player's name, and return his team
     params = {"page": 0, "per_page": 1, "search": player_name}
     response = requests.get(basketball_url, params)
     player_data = response.json()
@@ -25,7 +104,7 @@ def get_nba_player_team(player_name):  # # gets a player's name, and return his 
     return "could not find the player"
 
 # football functions:
-def get_footballer_team(player_name):  # gets a player's name, and return his team
+def get_footballer_team(player_name: str) -> str:  # gets a player's name, and return his team
 
     # get the player's id first
     football_url = f"https://www.thesportsdb.com/api/v1/json/{football_api_key}/searchplayers.php?p={player_name}"  
@@ -47,39 +126,63 @@ def get_footballer_team(player_name):  # gets a player's name, and return his te
     return team_name
 
 # dict to map user_id to his current state
-user_to_state = {}
+user_to_state = Cache()
 
 # handle a massage to the telegram bot:
 @bot.message_handler(func=lambda message: True) 
 def handle_message(message):
    
     user_id = message.from_user.id
-    if user_id not in user_to_state:
-        user_to_state[user_id] = {"is_sent_welcome": False, "is_chosen_football": False, "is_chosen_basketball": False}
+    if user_to_state.get_value(user_id) == -1:  # user is not in the cache
+        user_to_state.put_or_update(user_id, {"is_sent_welcome": False, "is_chosen_football": False, "is_chosen_basketball": False})
     
     answer = message.text
 
-    if not user_to_state[user_id]["is_sent_welcome"]:  # need to send a weolcome message
+    if not user_to_state.get_value(user_id)["is_sent_welcome"]:  # need to send a weolcome message
         bot.reply_to(message, "hello! please choose the sport you're intrested in: football or basketball")
-        user_to_state[user_id]["is_sent_welcome"] = True
+        user_to_state.put_or_update(user_id, {"is_sent_welcome": True, "is_chosen_football": False, "is_chosen_basketball": False})
 
     elif answer == "football":  # the user chose football
         bot.reply_to(message, "please provide a footballer's name, and i will provide his team")
-        user_to_state[user_id]["is_chosen_football"] = True
-        user_to_state[user_id]["is_chosen_basketball"] = False
+        user_to_state.put_or_update(user_id, {"is_sent_welcome": True, "is_chosen_football": True, "is_chosen_basketball": False})
     
     elif answer == "basketball":  # the user chose basketball
         bot.reply_to(message, "please provide a nba player's name, and i will provide his team")
-        user_to_state[user_id]["is_chosen_basketball"] = True
-        user_to_state[user_id]["is_chosen_football"] = False
+        user_to_state.put_or_update(user_id, {"is_sent_welcome": True, "is_chosen_football": False, "is_chosen_basketball": True})
         
-    elif user_to_state[user_id]["is_chosen_football"]:   # the user is sending a footballer's name 
+    elif user_to_state.get_value(user_id)["is_chosen_football"]:   # the user is sending a footballer's name 
         reply = get_footballer_team(answer)
         bot.reply_to(message, reply)
     
-    elif user_to_state[user_id]["is_chosen_basketball"]:   # the user is sending a nba player's name 
+    elif user_to_state.get_value(user_id)["is_chosen_basketball"]:   # the user is sending a nba player's name 
         reply = get_nba_player_team(answer)
         bot.reply_to(message, reply)
+
+
+def get_footballer_team_test(input: str, expceted_output: str):
+    # arrange:
+    # nothing to do here
+
+    # act:
+    res = get_footballer_team(input).lower()
+    
+    # assert:
+    assert res == expceted_output, "problem with input " + input + ", got " + res + ", expected " + expceted_output
+
+get_footballer_team_test("messi", "inter miami")
+
+
+def get_nba_player_team_test(input: str, expceted_output: str):
+    # arrange:
+    # nothing to do here
+
+    # act:
+    res = get_nba_player_team(input).lower()
+    
+    # assert:
+    assert res == expceted_output, "problem with input " + input + ", got " + res + ", expected " + expceted_output
+
+get_nba_player_team_test("lebron", "los angeles lakers")
 
 # keep the bot running
 if __name__ == '__main__':
